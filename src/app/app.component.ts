94218858e03700f9d1676f77f2b6c9b6cea684a1
Lucas Calje <lucas@192.168.2.11>
2020-07-03 22:07:34 +0200

    setup basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { stateBasic, IFbpNode } from '@scaljeri/fbp-core';
+import { IFbpMainReady } from '@scaljeri/fbp-ui';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit{
   title = 'app';
+  state = stateBasic;
+
+  @ViewChild('fbp') fbp: ElementRef;
+
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+      this.fbp.nativeElement.addEventListener('fbp-ready', ({detail}: { detail: IFbpMainReady}) => {
+        detail.init(this.state);
+    });
+  }
+
+  getChildNodes(node: IFbpNode): IFbpNode[] | null {
+    if (node.type) {
+      return null;
+    }
+
+    return Object.values(this.state.nodes).filter(childNode => childNode.parentId === node.parentId);
+  }
 }
